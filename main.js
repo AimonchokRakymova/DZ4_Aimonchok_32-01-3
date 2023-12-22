bed290624b8bdd6b8f181b747912ad12b28a02c1
AimonchokRakymova <aku.rkrk@gmail.com>
2023-12-22 17:58:19 +0600

    dz4

---
+++ main.js
@@ -0,0 +1,39 @@
+const cards = document.querySelector('.cards')
+
+const xhr = new XMLHttpRequest()
+xhr.open("GET", "peoples.json")
+xhr.setRequestHeader("Content-type", "application/json")
+xhr.send()
+
+
+xhr.addEventListener("load", () => {
+    const bio = JSON.parse(xhr.response)
+    console.log(bio)
+    bio.forEach((e) => {
+        const div = document.createElement('div')
+        div.classList.add('block1')
+        const span = document.createElement('span')
+        span.classList.add('span1')
+        span.innerHTML = e.name
+
+        const agespan = document.createElement('span')
+        agespan.classList.add('agespan1')
+        agespan.innerHTML = e.age
+
+        const img = document.createElement('img')
+        img.setAttribute("src", e.photo)
+
+        div.append(span, agespan, img);
+        cards.append(div)
+
+    })
+})
+
+const obj = new XMLHttpRequest()
+obj.open("GET", "main.json")
+obj.setRequestHeader("Content-type", "application/json")
+obj.send()
+
+obj.onload = () => {
+    console.log(JSON.parse(obj.response))
+}
